9b421295825055fe6e1546d0ec4eafbaf8fbd223
melnikov <melnikov@isida.by>
2023-03-28 12:34:17 +0300

    className

--- src/AddModal.js
+++ src/AddModal.js
@@ -36,25 +36,25 @@ const AddModal = ({ handleChange, newPaint, handleSubmit, validation, loseFocus,
                 <DialogTitle>{"Add new painting"}</DialogTitle>
                 <DialogContent>
                     <DialogContentText id="alert-dialog-slide-description">
-                        <form onChange={(e) => handleChange(e)}>
-                            <div class="row">
-                                <div class="col">
-                                    <input type="text" class="form-control mb-3" onFocus={(e) => { loseFocus(e.target.name) }} placeholder="Enter name" name="name" style={validation.name ? eStyle : {}} value={newPaint.name}></input>
+                        <div onChange={(e) => handleChange(e)}>
+                            <div className={"row"}>
+                                <div className={"col"}>
+                                    <input type="text" className={"form-control mb-3"} onFocus={(e) => { loseFocus(e.target.name) }} placeholder="Enter name" name="name" style={validation.name ? eStyle : {}} value={newPaint.name}></input>
                                     {validation.name && getMessage(validation.name)}
-                                    <input type="text" class="form-control mb-3" onFocus={(e) => { loseFocus(e.target.name) }} placeholder="Enter price" name="price" style={validation.price ? eStyle : {}} value={newPaint.price}></input>
+                                    <input type="text" className={"form-control mb-3"} onFocus={(e) => { loseFocus(e.target.name) }} placeholder="Enter price" name="price" style={validation.price ? eStyle : {}} value={newPaint.price}></input>
                                     {validation.price && getMessage(validation.price)}
-                                    <input type="text" class="form-control mb-3" placeholder="Enter image path" value={newPaint.path} name="path"></input>
-                                    <input type="text" class="form-control mb-3" placeholder="Enter category" value={newPaint.category} name="category"></input>
+                                    <input type="text" className={"form-control mb-3"} placeholder="Enter image path" value={newPaint.path} name="path"></input>
+                                    <input type="text" className={"form-control mb-3"} placeholder="Enter category" value={newPaint.category} name="category"></input>
                                 </div>
-                                <div class="col">
-                                    <input type="text" class="form-control mb-3" onFocus={(e) => { loseFocus(e.target.name) }} style={validation.year ? eStyle : {}} placeholder="Enter year" value={newPaint.year} name="year"></input>
+                                <div className={"col"}>
+                                    <input type="text" className={"form-control mb-3"} onFocus={(e) => { loseFocus(e.target.name) }} style={validation.year ? eStyle : {}} placeholder="Enter year" value={newPaint.year} name="year"></input>
                                     {validation.year && getMessage(validation.year)}
-                                    <input type="text" class="form-control mb-3" placeholder="Enter state" name="state" value={newPaint.state}></input>
-                                    <input type="text" class="form-control mb-3" placeholder="Enter author" name="author" value={newPaint.author}></input>
-                                    <input type="text" class="form-control mb-3" placeholder="Enter amount" name="amount" value={newPaint.amount}></input>
+                                    <input type="text" className={"form-control mb-3"} placeholder="Enter state" name="state" value={newPaint.state}></input>
+                                    <input type="text" className={"form-control mb-3"} placeholder="Enter author" name="author" value={newPaint.author}></input>
+                                    <input type="text" className={"form-control mb-3"} placeholder="Enter amount" name="amount" value={newPaint.amount}></input>
                                 </div>
                             </div>
-                        </form>
+                        </div>
                         <div style={{ textAlign: 'center' }}>
                             <button onClick={handleSubmit} className={'btn btn-primary'} style={{ width: '100px' }}>Save</button>
                         </div>
